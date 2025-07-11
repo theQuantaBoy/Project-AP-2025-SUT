8e8e628356a6eead5f75c30757b4120429ffb92d
BrokeArash <arashakbari2006@gmail.com>
2025-07-11 23:25:49 +0330

    menu label added

--- StardewValley/core/src/main/java/ap/project/screen/RegisterScreen.java
+++ StardewValley/core/src/main/java/ap/project/screen/RegisterScreen.java
@@ -3,6 +3,7 @@ package ap.project.screen;
 import ap.project.model.App.GameAssetsManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -16,38 +17,57 @@ public class RegisterScreen implements Screen {
     private Stage stage;
     private Image background;
     private Image logo;
-    private Table mainTable;
-    private Skin skin;
+    private Label menuName;
 
     public RegisterScreen() {
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(this.stage);
+
+        // Load assets
         this.background = new Image(GameAssetsManager.getGameAssetsManager().getRegisterBackground());
         this.background.setFillParent(true);
         this.logo = new Image(GameAssetsManager.getGameAssetsManager().getLogo());
-        this.mainTable = new Table();
+        this.menuName = new Label("REGISTRATION\nMENU", GameAssetsManager.getGameAssetsManager().getSkin());
+        this.menuName.setAlignment(Align.center);
+        this.menuName.setColor(Color.GOLD);
+        this.menuName.setFontScale(0.5f);
+
+
     }
 
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage);
-        stage.addActor(background);
-        logo.setPosition(stage.getWidth()/2 - logo.getWidth()/2, stage.getHeight() - logo.getHeight());
+        stage.addActor(this.background);
         stage.addActor(logo);
+        stage.addActor(menuName);
+        positionElements();
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float v) {
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act(delta);
+        Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
         stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
+        positionElements(); // Reposition elements on resize
+    }
+
+    private void positionElements() {
+        // Position logo at top center
+        float logoCenterX = stage.getWidth() / 2 - logo.getWidth() / 2;
+        float logoTopY = stage.getHeight() - logo.getHeight() - 20;
+        logo.setPosition(logoCenterX, logoTopY);
+
+        // Position label at top left
+        float labelLeftX = -10;
+        float labelTopY = stage.getHeight() - menuName.getHeight() - 20;
+        menuName.setPosition(labelLeftX, labelTopY);
     }
 
     @Override
