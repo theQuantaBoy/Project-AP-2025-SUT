a6cbe086af0717fcfb5f847952a9583d4d5da94d
Mohsen <mohsensalah128@gmail.com>
2025-08-10 20:29:13 +0330

    adding npc sprites

--- StardewValley/core/src/main/java/ap/project/model/enums/CharacterType.java
+++ StardewValley/core/src/main/java/ap/project/model/enums/CharacterType.java
@@ -9,6 +9,18 @@ public enum CharacterType
 
     ALEX("Alex", "characters/Alex/Alex_Sheet.atlas",
         "characters/Alex/Alex_Avatar.png", "characters/Alex/Alex_Profile.png"),
+
+    HARVEY("Harvey", "characters/npc/Harvey/Harvey_Sheet.atlas",
+        "characters/npc/Harvey/Harvey_Avatar.png", "characters/npc/Harvey/Harvey_Profile.png"),
+
+    LEAH("Leah", "characters/npc/Leah/Leah_Sheet.atlas",
+        "characters/npc/Leah/Leah_Avatar.png", "characters/npc/Leah/Leah_Profile.png"),
+
+    ROBIN("Robin", "characters/npc/Robin/Robin_Sheet.atlas",
+        "characters/npc/Robin/Robin_Avatar.png", "characters/npc/Robin/Robin_Profile.png"),
+
+    SEBASTIAN("Sebastian", "characters/npc/Sebastian/Sebastian_Sheet.atlas",
+        "characters/npc/Sebastian/Sebastian_Avatar.png", "characters/npc/Sebastian/Sebastian_Profile.png"),
     ;
 
     private final String name;
